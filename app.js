d0eeae77f325457e077b10c3950a69020aa73292
mohanpierce99 <paying.rich6990@gmail.com>
2018-12-08 00:03:28 +0530

    heroku

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express=require("express");
 const router=require("./router/routes.js");
 const app=express();
-
+const port=process.env.PORT||3000;
 router(app,express);
 
-app.listen(3000,'192.168.1.14');
+app.listen(port,()=>console.log("Server started"));
